02ad8b48c6be9ef396b4433b6c72daef73df7251
manish <mksaini1046@gmail.com>
2024-04-08 16:13:28 +0530

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>manish</span>
+      <span>© <a href="https://github.com/manishmksaini" target="_blank" >manishmksaini</a> Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
